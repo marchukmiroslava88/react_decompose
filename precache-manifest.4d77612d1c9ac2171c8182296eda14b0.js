a729389b39cdf5ede08fceb9a04b65d81c9686ea
Miroslava <777shadowpriest777@gmail.com>
2019-09-06 20:06:15 +0300

    Updates

---
+++ precache-manifest.4d77612d1c9ac2171c8182296eda14b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c70c249624ef853f7b8d8b0927d59b",
+    "url": "/marchukmiroslava88/react_decompose/index.html"
+  },
+  {
+    "revision": "2785216ce4b50fc3ccef",
+    "url": "/marchukmiroslava88/react_decompose/static/css/main.06b68361.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "/marchukmiroslava88/react_decompose/static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "2785216ce4b50fc3ccef",
+    "url": "/marchukmiroslava88/react_decompose/static/js/main.c2e52cfc.chunk.js"
+  },
+  {
+    "revision": "aed38026062a2ac96986",
+    "url": "/marchukmiroslava88/react_decompose/static/js/runtime~main.3ee32ee0.js"
+  }
+]);
